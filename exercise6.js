67c70eae366661fc2ebc5ca0389443097bdf3c19
littlehorseboy <littlehorseboy@gmail.com>
2020-04-04 13:57:22 +0800

    feat: 補上 if (err)

--- exercise6.js
+++ exercise6.js
@@ -1,6 +1,9 @@
 const searchExt = require('./exercise6.searchExt');
 
 searchExt(process.argv[2], process.argv[3], (err, filteredFiles) => {
+  if (err) {
+    console.error(err);
+  }
   filteredFiles.forEach((file) => {
     console.log(file);
   });
